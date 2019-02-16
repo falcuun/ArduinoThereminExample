9f80d1e73a6900734cd311b3a8a90a1482dee6d7
falcuun <falcuun@gmail.com>
2019-02-16 01:52:28 +0100

    Added Ultra Sonic Sensor distance detection

--- ArduinoTheremin.ino
+++ ArduinoTheremin.ino
@@ -1,6 +1,25 @@
-void setup(){
+const int SENSOR1_TRIG = 2;
+const int SENSORT1_ECHO = 3;
+const int BUZZER1_PIN = A0;
 
+void setup(){
+    pinMode(SENSOR1_TRIG, OUTPUT);
+    pinMode(SENSORT1_ECHO, INPUT);
+
+    Serial.begin(9600);
 }
+
 void loop(){
-    
+
+    digitalWrite(SENSOR1_TRIG, LOW);
+    delayMicroseconds(5);
+    digitalWrite(SENSOR1_TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(SENSOR1_TRIG, LOW);
+
+    int duration = pulseIn(SENSORT1_ECHO, HIGH);
+    int distance_CM = duration / 58;
+
+    delay(10);
+
 }
